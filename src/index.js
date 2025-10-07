ff699da0f88133b25235516c7d34d7cebcceded5
Mert <espawnn@gmail.com>
2025-10-07 15:10:30 +0300

    Project v1.1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom/client";
+import './index.css';
 import App from "./components/app.js";
 import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
